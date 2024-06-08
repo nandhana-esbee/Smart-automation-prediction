a4b4176bb57fb2160f39d029f9ec2f6712018faa
Nandhana SB <nandhanaesbee16@gmail.com>
2024-06-09 03:56:50 +0530

    Created a django app

---
+++ smart_automation/utils/model_train/automatedai.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from datetime import datetime, timezone
+
+# Load data
+df = pd.read_csv('data/Temperature.csv')
+df = df.dropna(axis=1)
+
+#only take _time,_value and _start columns
+df = df[['_time','_value','_start']]
+df = df.dropna()
+
+df['_time'] = pd.to_datetime(df["_time"])
+df['_time'] = df['_time'].dt.tz_localize(None)
+
+df['Time_Interval'] = df['_time'].diff().dt.total_seconds() / 3600  
+df['Time_Interval'] = df['Time_Interval'].fillna(0)  # Replace NaN with 0
+
+X = df["Time_Interval"] # Features (e.g., time of day))
+y = df["_value"]      # Target values (e.g., temperature)
+datafit = pd.DataFrame(dict(x=X, y=y))
+datafit = datafit.dropna()
+
+
+# #Create and fit the linear regression model
+model = LinearRegression()
+model.fit(datafit[["x"]], datafit["y"])
+df = df.dropna()
+
+#save model
+import joblib
+joblib.dump(model, 'models/temperature_model.joblib')
+print("success")
---
+++ smart_automation/utils/temperature_pred.py
@@ -0,0 +1,22 @@
+
+from datetime import datetime, timezone
+import pandas as pd
+import joblib
+
+def temp_pred(date_time):
+    #load the model
+    model = joblib.load('models/temperature_model.joblib')
+
+    # Predict temperature for a new data point
+    current_time = datetime.now(timezone.utc)
+
+    date_time =  datetime.fromisoformat(date_time)
+    #print(date_time)
+    diff = (current_time-date_time).total_seconds()/3600
+    predicted_temperature = model.predict([[diff]])
+    return predicted_temperature[0]
+
+
+
+print(temp_pred("2024-09-01T00:00:00Z"))
+#2023-01-20 01:16:00 
