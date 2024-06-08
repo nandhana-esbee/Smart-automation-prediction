13c9e250317634a06379f4b6f1a0a9b64c52133a
Nandhana SB <nandhanaesbee16@gmail.com>
2024-06-09 02:14:39 +0530

    model trained with very low accuracy

--- automatedai.py
+++ automatedai.py
@@ -1,36 +1,41 @@
-from sklearn.model_selection import train_test_split
 import pandas as pd
+from datetime import datetime ,timezone
 
 # Load data
 df = pd.read_csv('data/Temperature.csv')
-#print(df)
 
 from sklearn.linear_model import LinearRegression
 
 # Sample data (replace with your actual data)
 df['_time'] = pd.to_datetime(df["_time"])
 df['Time_Interval'] = df['_time'].diff().dt.total_seconds() / 3600  
-#print(df["Time_Interval"])
 
 X = df["Time_Interval"]  # Features (e.g., time of day))
 y = df["_value"]      # Target values (e.g., temperature)
 datafit = pd.DataFrame(dict(x=X, y=y))
 datafit = datafit.dropna()
-#print(df.dtypes)
+
 
 # #Create and fit the linear regression model
 model = LinearRegression()
 model.fit(datafit[["x"]], datafit["y"])
 print("success")
-# # Predict temperature for a new data point
-# df["_start"] = pd.to_datetime(df["_start"])
-# for time in df["_start"]:
-#    new_data = time
-#    #print(new_data)
-#    predicted_temperature = model.predict(new_data)
 
-#    # Print the predicted temperature
-#    print(f"Predicted temperature for time {new_data} :{predicted_temperature}")
+
+# Predict temperature for a new data point
+current_time = datetime.now(timezone.utc)
+
+df["_start"] = pd.to_datetime(df["_start"])
+df['time_difference_seconds'] = (current_time - df['_start']).dt.total_seconds()/3600
+#print(df['time_difference_seconds'])
+
+for time in df["time_difference_seconds"]:
+    break
+print(time)
+print(model.predict([[time]])[0])
+    #predicted_temperature = model.predict(time)
+    # Print the predicted temperature
+    #print(f"Predicted temperature  :{predicted_temperature}")
 
 
 
